901562f05935fe557612e79eee13f365b5ea9a87
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2021-08-28 17:51:02 +0530

    docs
    added javascript logic to make the todo list work

---
+++ index.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('input');
+const list = document.querySelector('.todo-list');
+const button = document.querySelector('.add');
+let checkBox = document.querySelectorAll('input[type=checkbox]');
+let arr = Array.from(checkBox);
+
+function add() {
+	const newLi = document.createElement('li');
+	const newCheckBox = document.createElement('input');
+	newCheckBox.type = 'checkbox';
+	if (input.value.length > 0) {
+		newLi.innerHTML = input.value;
+		list.append(newLi);
+		newLi.append(newCheckBox);
+		arr.push(newCheckBox);
+		input.value = '';
+		remove();
+	}
+}
+
+button.addEventListener('click', () => {
+	add();
+});
+
+input.addEventListener('keydown', (e) => {
+	if (e.code === 'Enter') {
+		add();
+	}
+});
+
+function remove() {
+	arr.forEach((el) =>
+		el.addEventListener('click', (select) => {
+			select.target.parentElement.remove();
+			checkBox = document.querySelectorAll('input[type=checkbox]');
+			arr = Array.from(checkBox);
+		})
+	);
+}
+
+remove();
